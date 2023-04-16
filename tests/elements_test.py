1485a7f45833b69580b10e8562ed457c5d519761
Stas Cherepanov <94304561+zaelcovsky@users.noreply.github.com>
2023-04-17 00:22:41 +0700

    Added tests for textbox page and checkbox page

---
+++ tests/elements_test.py
@@ -0,0 +1,31 @@
+import time
+from pages.elements_page import *
+
+
+class TestElements:
+    class TestTextBox:
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
+            text_box_page.open()
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_field()
+            out_name, out_email, out_current_address, out_permanent_address = text_box_page.check_filled_field()
+            time.sleep(5)
+            assert full_name == out_name
+            assert email == out_email
+            assert current_address == out_current_address
+            assert permanent_address == out_permanent_address
+
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, "https://demoqa.com/checkbox")
+            check_box_page.open()
+            check_box_page.open_full_list()
+            time.sleep(3)
+            check_box_page.click_random()
+            input_checkbox = check_box_page.get_checked_box()
+            output_checkbox = check_box_page.get_output_result()
+            time.sleep(3)
+            print(input_checkbox)
+            print(output_checkbox)
+            assert input_checkbox == output_checkbox
+
